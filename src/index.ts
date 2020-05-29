35db7bc906b343cdfce7f8570108d1d99644da01
Tommy <tommy@viq.app>
2020-05-30 01:19:10 +0200

    Connect blockchain to a redis pubsub class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-import './average-work'
+import express, { Request, Response } from 'express'
+import { Blockchain } from '@/blockchain'
+import { PubSub } from '@/pubsub'
+
+const app = express()
+const blockchain = new Blockchain()
+const pubsub = new PubSub({ blockchain })
+
+setTimeout(() => {
+  pubsub.broadcastChain()
+}, 1000)
+
+app.use(express.json())
+
+app.get('/api/blocks', (req: Request, res: Response) => {
+  res.send(blockchain.chain)
+})
+
+app.post('/api/mine', (req: Request, res: Response) => {
+  const { data } = req.body
+  blockchain.addBlock({ data })
+
+  res.redirect('/api/blocks')
+})
+
+const PORT = 4000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
---
+++ src/pubsub.ts
@@ -0,0 +1,62 @@
+import redis from 'redis'
+import { Blockchain } from '@/blockchain'
+
+interface PubSubProps {
+  blockchain: Blockchain
+}
+interface PublishProps {
+  channel: string
+  message: any
+}
+
+const CHANNELS = {
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+}
+
+export class PubSub {
+  blockchain: Blockchain
+
+  publisher: redis.RedisClient
+  subscriber: redis.RedisClient
+
+  constructor({ blockchain }: PubSubProps) {
+    this.blockchain = blockchain
+    this.publisher = redis.createClient()
+    this.subscriber = redis.createClient()
+
+    this.subscribeToChannels()
+
+    this.subscriber.on('message', (channel, message) =>
+      this.handleMessage(channel, message),
+    )
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach((channel) => {
+      this.subscriber.subscribe(channel)
+    })
+  }
+
+  publish({ channel, message }: PublishProps) {
+    this.publisher.publish(channel, message)
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    })
+  }
+
+  handleMessage(channel: string, message: any) {
+    console.log(`[ ${channel} ] Message received ->> ${message}`)
+
+    const parsedMessage = JSON.parse(message)
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      console.log(this.blockchain)
+      this.blockchain.replace(parsedMessage)
+    }
+  }
+}
